fef9c66bb29041dc07d82a4ccee173ca6f49ba3d
Quinten Transé <quinten.trans@outlook.com>
2022-04-27 14:24:40 +0200

    interface

---
+++ wire-reelcutter/wire-reelcutter/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace wire_reelcutter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string com = "";
+            Console.WriteLine("What port is your mbed connected to?");
+            com = Console.ReadLine();
+            SerialPort serialPort = new SerialPort("COM"+com, 9600, Parity.None, 8, StopBits.One);
+            serialPort.Open();
+            Console.WriteLine("open");
+            char[] buffer = new char[12];
+            //serialPort.Read(buffer, 0, 4);
+            //Console.WriteLine("received");
+            //Console.WriteLine(buffer);
+            //serialPort.Write("hello");
+            //serialPort.Read(buffer, 0, 5);
+            //Console.WriteLine("received");
+            //Console.WriteLine(buffer);
+            string confirmation = "n";
+            while (confirmation == "n" || confirmation == "N")
+            {
+                string cutter = "";
+                while (!(cutter == "w" || cutter == "W" || cutter == "r" || cutter == "R"))
+                {
+                    Console.WriteLine("Do you want to use the reelcutter or wirecutter?");
+                    Console.WriteLine("Type W for wirecutter");
+                    Console.WriteLine("Type R for reelcutter");
+                    cutter = Console.ReadLine();
+                }
+                if (cutter == "W" || cutter == "w")
+                {
+                    Console.Write("What length of the first part needs to be stripped? (in mm) ");
+                    int leftStripped = Convert.ToInt32(Console.ReadLine());
+                    
+                    Console.WriteLine("received");
+                    Console.WriteLine(buffer);
+                    Console.Write("What length of the second part needs to be stripped? (in mm) ");
+                    int rightStripped = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("received");
+                    Console.WriteLine(buffer);
+                    Console.Write("What's the middle length of the wire? (in mm) ");
+                    int middleLength = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("received");
+                    Console.WriteLine(buffer);
+                    Console.Write("What amount of wires need to be stripped? ");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    serialPort.Write($"{leftStripped}{rightStripped}{middleLength}{amount}");
+                    serialPort.Read(buffer, 0, 12);
+                    Console.WriteLine("received");
+                    Console.WriteLine(buffer);
+                    Console.WriteLine($"First side is stripped {leftStripped} mm.");
+                    Console.WriteLine($"Second side is stripped {rightStripped} mm.");
+                    Console.WriteLine($"The wire has a middle length of {middleLength} mm.");
+                    Console.WriteLine($"There are a total of {amount} wires to be cut");
+                    Console.WriteLine("Is this what you wanted? [Y/n]");
+                    confirmation = Console.ReadLine();
+                }
+                else if (cutter == "R" || cutter == "r")
+                {
+                    int reelnumber = 0;
+                    while (!(reelnumber == 1 || reelnumber == 2 || reelnumber == 3 || reelnumber == 4))
+                    {
+                        Console.WriteLine("What size will you use? ");
+                        Console.WriteLine("Enter '1' for 0402 ");
+                        Console.WriteLine("Enter '2' for 0603 ");
+                        Console.WriteLine("Enter '3' for 0805 ");
+                        Console.WriteLine("Enter '4' for 1206 ");
+                        reelnumber = Convert.ToInt32(Console.ReadLine());
+                        serialPort.Write($"{reelnumber}");
+                        serialPort.Read(buffer, 0, 3);
+                        Console.WriteLine("received");
+                        Console.WriteLine(buffer);
+                    }
+                    if (reelnumber == 1 || reelnumber == 2 || reelnumber == 3 || reelnumber == 4)
+                    {
+                        Console.Write("How many components do you want per reel? ");
+                        int numberOfComponents = Convert.ToInt32(Console.ReadLine());
+                        serialPort.Write($"{numberOfComponents}");
+                        serialPort.Read(buffer, 0, 3);
+                        Console.WriteLine("received");
+                        Console.WriteLine(buffer);
+                        Console.Write("How many reels do you want? ");
+                        int numberOfReels = Convert.ToInt32(Console.ReadLine());
+                        serialPort.Write($"{numberOfReels}");
+                        serialPort.Read(buffer, 0, 3);
+                        Console.WriteLine("received");
+                        Console.WriteLine(buffer);
+                        Console.WriteLine($"There are a total of {numberOfComponents} components per reel.");
+                        Console.WriteLine($"There are a total of {numberOfReels} reels to be cut.");
+                        Console.WriteLine("Is this what you wanted? [Y/n]");
+                        confirmation = Console.ReadLine();
+                    }
+                }
+
+            else
+            {
+                Console.WriteLine("That is not an option...");
+            }
+            }
+        }
+        }
+    }
+
